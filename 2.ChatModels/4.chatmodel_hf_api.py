6a16ab6908e2ab04a61581fc41b58f341cb0312a
Abhay Srivastav <148490972+AbhaySrivastav18@users.noreply.github.com>
2026-06-10 16:41:08 +0530

    Update HuggingFace model and query in chat model

--- 2.ChatModels/4.chatmodel_hf_api.py
+++ 2.ChatModels/4.chatmodel_hf_api.py
@@ -1,14 +1,14 @@
-from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 
 load_dotenv()
 
-
-llm = HuggingFaceEndpoint (
-    repo_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
     task="text-generation"
 )
 
 model = ChatHuggingFace(llm=llm)
-res = model.invoke("Who is the prime minister of india")
+
+res = model.invoke("What is the capital of India?")
 print(res.content)
